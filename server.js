f740b9491a597086b9526914bd3a44736e1ee8ba
Giedrius <lauruska.giedrius@gmail.com>
2019-06-13 08:50:11 +0300

    initial commit
    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API Runnning'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
