bb4499463e050e310fe266a56e7fe295389997ab
Giedrius <lauruska.giedrius@gmail.com>
2019-06-19 22:59:54 +0300

    Added routes to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,25 +16,85 @@ router.post('/', [auth,
         check('text', 'Text is required').not().isEmpty(),
     ]
 ], wrap(async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        }
-
-        const user = await User.findById(req.user.id).select('-password');
-
-        const newPost = new Post({
-            text: req.body.text,
-            name: user.name,
-            avatar: user.avatar,
-            user: req.user.id
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
         });
+    }
 
-        const post = await newPost.save();
+    const user = await User.findById(req.user.id).select('-password');
 
-        res.json(post);
-    }));
+    const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+    });
+
+    const post = await newPost.save();
+
+    res.json(post);
+}));
+
+
+router.get('/', auth, wrap(async(req, res) => {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+}));
+
+
+router.get('/:id', auth, wrap(async(req, res) => {
+    const posts = await Post.findById(req.params.id);
+
+    if (!posts) res.status(404).json({ msg: 'Post not found' });
+    res.json(posts);
+}));
+
+
+router.delete('/:id', auth, wrap(async(req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if (post.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User not authorised' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+}));
+
+
+router.put('/like/:id', auth, wrap(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    
+    await post.save();
+
+    res.json(post.likes);
+}));
+
+router.put('/unlike/:id', auth, wrap(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+        return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+}));
+
 
 module.exports = router;
