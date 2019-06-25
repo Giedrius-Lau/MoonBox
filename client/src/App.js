91ec50be34a09ff1f0f16d49c01aad3858157dbf
Giedrius <lauruska.giedrius@gmail.com>
2019-06-25 09:38:54 +0300

    Updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import setAuthToken from './utils/setAuthToken';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
-const App = () => { 
-	useEffect(() => {
-		store.dispatch(loadUser());
-	}, []);
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-	return (
-		<Provider store={store}>
-			<Router>
+  return (
+    <Provider store={store}>
+      <Router>
 				<Navbar />
 				<Route exact path='/' component={Landing} />
 				<section className="container">
@@ -36,7 +36,8 @@ const App = () => {
 					</Switch>
 				</section>
 			</Router>
-		</Provider>
-)}
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,67 +1,72 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
-const Login = ({ login, isAuthenticaded }) => {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
 
-    const { email, password } = formData;
+  const { email, password } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = async e => {
-        e.preventDefault();
-        login(email, password);
-    };
+  const onSubmit = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    console.log(isAuthenticaded)
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
-    if (isAuthenticaded) {
-        return <Redirect to="/dashboard" />
-    }
-
-    return (
-        <Fragment>
-            <h1 className="large text-primary">Sign Up</h1>
-            <p className="lead"><i className="fas fa-user"></i> Sign in Your Account</p>
-            <form className="form" onSubmit={e => onSubmit(e)}>
-                <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" required value={email} onChange={e => onChange(e)}/>
-                </div>
-
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        minLength="6"
-                        required 
-                        value={password} 
-                        onChange={e => onChange(e)}
-                    />
-                </div>
-
-                <input type="submit" className="btn btn-primary" value="Login" />
-            </form>
-            <p className="my-1">
-                Dont have an account? <Link to="/register">Sign Up</Link>
-            </p>
-        </Fragment>
-    )
-}
-
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
 Login.propTypes = {
-    login: PropTypes.func.isRequired,
-    isAuthenticaded: PropTypes.bool,
-}
-
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticaded,
-})
-
-export default connect(mapStateToProps, { login })(Login);
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
