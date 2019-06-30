dd6044709c55ba190c68b35f8ba9ca26409e7dfc
Giedrius <lauruska.giedrius@gmail.com>
2019-06-30 13:31:49 +0300

    Added education adn experience to profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './../profile/ProfileTop';
 import ProfileAbout from './../profile/ProfileAbout';
+import ProfileExperience from './../profile/ProfileExperience';
+import ProfileEducation from './../profile/ProfileEducation';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 	useEffect(() => {
@@ -30,6 +32,30 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 					<div class="profile-grid my-1">
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className="profile-exp bg-white p-2">
+							<h2 className="text-primary">Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map(experience => (
+										<ProfileExperience key={experience._id} experience={experience} />
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience credentials</h4>
+							)}
+						</div>
+						<div className="profile-edu bg-white p-2">
+							<h2 className="text-primary">Education</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map(education => (
+										<ProfileEducation key={education._id} education={education} />
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience credentials</h4>
+							)}
+						</div>
 					</div>
 				</Fragment>
 			)}
