95fd9ccccaedefde2d504fcd0dcaddb790a3421f
Giedrius <lauruska.giedrius@gmail.com>
2019-07-03 07:25:14 +0300

    Added posts form

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
 
 export const getPosts = () => async dispatch => {
 	try {
@@ -27,11 +27,10 @@ export const addLike = id => async dispatch => {
 			payload: { id, likes: res.data }
 		});
 	} catch (err) {
-		console.log(err.message);
-		// dispatch({
-		// 	type: POST_ERROR,
-		// 	payload: { msg: err.response.statusText, status: err.response.status }
-		// });
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
 	}
 };
 
@@ -44,10 +43,51 @@ export const removeLike = id => async dispatch => {
 			payload: { id, likes: res.data }
 		});
 	} catch (err) {
-		console.log(err.message);
-		// dispatch({
-		// 	type: POST_ERROR,
-		// 	payload: { msg: err.response.statusText, status: err.response.status }
-		// });
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+export const deletePost = id => async dispatch => {
+	try {
+		await axios.delete(`/api/posts/${id}`);
+
+		dispatch({
+			type: DELETE_POST,
+			payload: id
+		});
+
+		dispatch(setAlert('Post Removed', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+export const addPost = formData => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	try {
+		const res = await axios.post(`/api/posts/`, formData, config);
+
+		dispatch({
+			type: ADD_POST,
+			payload: res.data
+		});
+
+		dispatch(setAlert('Post Created', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
 	}
 };
